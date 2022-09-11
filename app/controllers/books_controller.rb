aa90912a4086257769c86389f88319165286555f
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-09-11 16:28:38 +0000

    [Update]4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,13 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
   end
-  
+
   def index
     @books = Book.all
   end
@@ -18,10 +18,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  private
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
