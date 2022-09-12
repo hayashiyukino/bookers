ab017514b366acd64d84bc3dde28a6fa00c51b86
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-09-12 05:43:49 +0000

    [Update]4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
       if @book.save
+        flash[:notice] = "Book was successfully created."
         redirect_to book_path(@book.id)
-      # else
-      #   render :new
+      else　#↓アプリケーションを完成させよう9章の内容↓
+         @books = Book.all 
+         render :index  #投稿一覧ページを再表示
       end
   end
 
@@ -26,9 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:notice] = "Book was successfully updated."
+        redirect_to book_path(book.id)
+      else
+        render :edit
+      end
   end
   
   def destroy
